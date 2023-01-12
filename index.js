e0a6a2a525027d0533f8933fa17b078b2e7b5f3c
blubyu <ramida470@gmail.com>
2023-01-12 20:32:44 +0700

    added

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+require("dotenv").config()
+const PORT = process.env.PORT
+app.get("/", (req, res) => {
+    res.send("<h1>hey hi</h1>")
+})
+app.listen(PORT, () => {
+    console.log("server startd");
+})
